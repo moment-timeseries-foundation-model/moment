9856b4e6da42d41f806c6f57a503a91b6531ceb7
mciccozzi-ah <matteo.ciccozzi@earnin.com>
2025-10-20 11:21:12 -0700

    Bump version to 0.1.5, prepare for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="momentfm",
-    version="0.1.2",
+    version="0.1.5",
     description="MOMENT: A Family of Open Time-series Foundation Models",
     author="Mononito Goswami, Konrad Szafer, Arjun Choudhry, Yifu Cai, Shuo Li, Artur Dubrawski",
     author_email="mgoswami@andrew.cmu.edu",
